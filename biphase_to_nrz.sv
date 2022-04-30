26a8fb95441044aef7f72773e70e468bd4cd57b0
Douglas P. Fields, Jr <symbolics@lisp.engineer>
2022-04-30 12:11:38 -0400

    Wrote up my (previously handwritten) algorithm for biphase to NRZ

--- biphase_to_nrz.sv
+++ biphase_to_nrz.sv
@@ -20,6 +20,44 @@
 //
 // NRZ data could be used as a UART input for further decoding, for example.
 
+// The algorithm:
+//
+// Whenever the input changes:
+//   1. Reset a counter to zero; reset counter_overflow
+//   2. Check the counter's (previous) value:
+//      a. > 3x short pulse length OR counter_overflow
+//               SET framing_error
+//               CLEAR short_seen
+//      b. > 2x short pulse length - long pulse
+//               SET data_received
+//               SET data 1 (marking)
+//               IF short_seen SET framing_error
+//               CLEAR short_seen
+//               TOGGLE clock_out
+//      c. > short & NOT short_seen
+//               SET short_seen
+//      d. > short & short_seen
+//               SET data_received
+//               SET data 0 (spacing)
+//               CLEAR short_seen
+//               TOGGLE clock_out
+//      e. < ignore pulse length
+//               SET glitch_seen
+//      f. OTHERWISE (> ignore, < short)
+//               SET framing_error
+//               CLEAR short_seen
+//
+// Whenever the input doesn't change:
+//   1. Increment the counter
+//           SET counter_overflow if the counter overflows
+//   2. Reset the pulses
+//           CLEAR framing_error
+//           CLEAR data_received
+//           CLEAR glitch_seen
+
+// Feed the input through a synchronizer as it is likely
+// coming from the outside world and not aligned with our system clock.
+
 module biphase_to_nrz (
 );
 
